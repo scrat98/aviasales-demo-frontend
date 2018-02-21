4154fd69d3b192297109d96fbb4ab9668f1cb755
scrat98 <artemgolovko98@gmail.com>
2018-02-21 13:09:55 +0300

    extract category to component

--- src/Main/TopDestinations/Categories.jsx
+++ src/Main/TopDestinations/Categories.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Grid, Col, Row } from "react-flexbox-grid";
+import Category from "./Category";
 
 import world from "./icons/world.svg";
 import beach from "./icons/beach.svg";
@@ -13,42 +14,6 @@ const Wrapper = styled.div`
   margin-bottom: 2rem;
 `;
 
-const CategoryTitle = styled.h3`
-  font-family: Roboto;
-  padding: 0 0.3rem;
-  font-style: normal;
-  font-weight: normal;
-  line-height: 1.25rem;
-  font-size: 0.75rem;
-  text-align: center;
-  text-transform: uppercase;
-`;
-
-const Category = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 1rem;
-  cursor: ${props => (props.isActive ? "auto" : "pointer")};
-
-  ${CategoryTitle} {
-    color: ${props => (props.isActive ? "#5C5C5C" : "#00ace2")};
-    border-bottom: ${props => (props.isActive ? "1px solid #00ace2" : "none")};
-  }
-`;
-
-const CategoryImgWrapper = styled.div`
-  border-radius: 50%;
-  display: flex;
-  align-item: center;
-  justify-content: center;
-  background: #ffffff;
-  box-shadow: 0px 4px 16px rgba(74, 74, 74, 0.12);
-  width: 50px;
-  height: 50px;
-`;
-
 export default () => (
   <Wrapper>
     <Grid>
@@ -56,52 +21,22 @@ export default () => (
         <Col xs={12} md={10} lg={6} mdOffset={1} lgOffset={3}>
           <Row center="xs">
             <Col xs={4} md={2}>
-              <Category isActive>
-                <CategoryImgWrapper>
-                  <img src={world} alt="world" />
-                </CategoryImgWrapper>
-                <CategoryTitle>Куда угодно</CategoryTitle>
-              </Category>
+              <Category active img={world} alt="world" name="Куда угодно" />
             </Col>
             <Col xs={4} md={2}>
-              <Category>
-                <CategoryImgWrapper>
-                  <img src={beach} alt="beach" />
-                </CategoryImgWrapper>
-                <CategoryTitle>Солнце и море</CategoryTitle>
-              </Category>
+              <Category img={beach} alt="beach" name="Солнце и море" />
             </Col>
             <Col xs={4} md={2}>
-              <Category>
-                <CategoryImgWrapper>
-                  <img src={shop} alt="shop" />
-                </CategoryImgWrapper>
-                <CategoryTitle>Шопинг, город</CategoryTitle>
-              </Category>
+              <Category img={shop} alt="shop" name="Шопинг, город" />
             </Col>
             <Col xs={4} md={2}>
-              <Category>
-                <CategoryImgWrapper>
-                  <img src={history} alt="history" />
-                </CategoryImgWrapper>
-                <CategoryTitle>Культура и история</CategoryTitle>
-              </Category>
+              <Category img={history} alt="history" name="Культура и история" />
             </Col>
             <Col xs={4} md={2}>
-              <Category>
-                <CategoryImgWrapper>
-                  <img src={party} alt="party" />
-                </CategoryImgWrapper>
-                <CategoryTitle>Ночная жизнь</CategoryTitle>
-              </Category>
+              <Category img={party} alt="party" name="Ночная жизнь" />
             </Col>
             <Col xs={4} md={2}>
-              <Category>
-                <CategoryImgWrapper>
-                  <img src={children} alt="children" />
-                </CategoryImgWrapper>
-                <CategoryTitle>Отдых с детьми</CategoryTitle>
-              </Category>
+              <Category img={children} alt="children" name="Отдых с детьми" />
             </Col>
           </Row>
         </Col>
---
+++ src/Main/TopDestinations/Category.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const CategoryTitle = styled.h3`
+  font-family: Roboto;
+  padding: 0 0.3rem;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 1.25rem;
+  font-size: 0.75rem;
+  text-align: center;
+  text-transform: uppercase;
+  color: ${props => (props.active ? "#5C5C5C" : "#00ace2")};
+  border-bottom: ${props => (props.active ? "1px solid #00ace2" : "none")};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 1rem;
+  cursor: ${props => (props.active ? "auto" : "pointer")};
+`;
+
+const CategoryImgWrapper = styled.div`
+  border-radius: 50%;
+  display: flex;
+  align-item: center;
+  justify-content: center;
+  background: #ffffff;
+  box-shadow: 0px 4px 16px rgba(74, 74, 74, 0.12);
+  width: 50px;
+  height: 50px;
+`;
+
+export default ({ img, alt, active, name }) => (
+  <Wrapper>
+    <CategoryImgWrapper>
+      <img src={img} alt={alt} />
+    </CategoryImgWrapper>
+    <CategoryTitle active={active}>{name}</CategoryTitle>
+  </Wrapper>
+);
